2a5ed7105b71401034e798e721d9443e3c9ac47b
rxby <nil@rxby.dev>
2026-04-12 08:45:10 +0900

    git integration

--- init.lua
+++ init.lua
@@ -29,6 +29,8 @@ Plug('HiPhish/rainbow-delimiters.nvim')
 
 Plug('f-person/auto-dark-mode.nvim')
 
+Plug('lewis6991/gitsigns.nvim')
+
 vim.call('plug#end')
 
 
@@ -88,14 +90,34 @@ require("nvim-tree").setup({
   renderer = {
     icons = {
       show = {
-        file = false,
-        folder = false,
+        file = true,
+        folder = true,
         folder_arrow = false,
-        git = false,
+        git = true,
         modified = false,
         hidden = false,
         diagnostics = false,
         bookmarks = false
+      },
+      glyphs = {
+        default = " ",
+        symlink = " ",
+        git = {
+          unstaged = "U",
+          staged = "S",
+          unmerged = "M",
+          renamed = "R",
+          deleted = "D",
+          untracked = "X",
+          ignored = "I"
+        },
+        folder = {
+          default = " ",
+          open = " ",
+          empty = " ",
+          empty_open = " ",
+          symlink = " ",
+        },
       }
     }
   }
@@ -200,7 +222,56 @@ vim.g.rainbow_delimiters = { highlight = {
   "RainbowGreen",
 }}
 
-
+require('gitsigns').setup {
+  signs = {
+    add          = { text = '┃' },
+    change       = { text = '┃' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '┆' },
+  },
+  signs_staged = {
+    add          = { text = '┃' },
+    change       = { text = '┃' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '┆' },
+  },
+  signs_staged_enable = true,
+  signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+  numhl      = true, -- Toggle with `:Gitsigns toggle_numhl`
+  linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+  word_diff  = true, -- Toggle with `:Gitsigns toggle_word_diff`
+  watch_gitdir = {
+    follow_files = true
+  },
+  auto_attach = true,
+  attach_to_untracked = false,
+  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    delay = 1000,
+    ignore_whitespace = false,
+    virt_text_priority = 100,
+    use_focus = true,
+  },
+  current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+  blame_formatter = nil, -- Use default
+  sign_priority = 6,
+  update_debounce = 100,
+  status_formatter = nil, -- Use default
+  max_file_length = 40000, -- Disable if file is longer than this (in lines)
+  preview_config = {
+    -- Options passed to nvim_open_win
+    style = 'minimal',
+    relative = 'cursor',
+    row = 0,
+    col = 1
+  },
+}
 
 
 function TabLineCell(i)
@@ -240,6 +311,7 @@ vim.opt.cursorline = true
 vim.opt.scl = 'no'
 vim.opt.laststatus = 3
 vim.opt.clipboard = "unnamedplus"
+vim.opt.signcolumn = 'yes'
 vim.opt.colorcolumn = "80"
 vim.opt.equalalways = false
 --vim.o.termguicolors = true
