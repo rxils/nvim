6bad99a644512e418d3979d640fe8d997c34d03a
rxby <nil@rxby.dev>
2026-04-13 15:36:39 +0900

    mason

---
+++ lua/plugins/lsp/init.lua
@@ -0,0 +1,5 @@
+return {
+  require('plugins.lsp.nvim-lspconfig'),
+  require('plugins.lsp.mason'),
+  require('plugins.lsp.mason-lspconfig')
+}
---
+++ lua/plugins/lsp/mason-lspconfig.lua
@@ -0,0 +1,16 @@
+return {
+  'mason-org/mason-lspconfig.nvim',
+
+  dependencies = {
+    { 'mason-org/mason.nvim', opts = {} },
+    'neovim/nvim-lspconfig',
+  },
+
+  opts = {
+    ensure_installed = {
+      'lua_ls',
+      'vimls',
+      'kotlin_lsp'
+    }
+  }
+}
